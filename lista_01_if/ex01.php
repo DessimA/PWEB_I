0ec90d874e8f27aa06a00d1aee7ce23f34f4c186
DessimA <60760405+DessimA@users.noreply.github.com>
2020-02-06 19:57:37 -0300

    Add files via upload

---
+++ lista_01_if/ex01.php
@@ -0,0 +1,19 @@
+<?php
+
+$Numero = $_GET["numero"];
+
+if($NumeroNum % 10 == 0){
+        echo "$Numero é divisivel por 10";
+    }
+    else if($Numero % 5 == 0){
+        echo "$Numero é divisivel por 5";
+    }
+    else if($Numero % 2 == 0){
+        echo "$Numero é divisivel por 2.";
+    }
+    else
+    {
+        echo "$Numero nao e divisivel por 10, 5 ou 2.";
+    }
+
+
---
+++ lista_01_if/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+         
+        <form action="ex01.php" method="get">
+            Digite um número inteiro: <input type="int" name="numero">
+</form>
+        
+         <form action="ex02.php" method="get">
+            Número 01: <input type="int" name="n1">
+            Número 02: <input type="int" name="n1">
+</form>
+        
+         <form action="ex03.php" method="get">
+            Nome: <input type="text" name="nomecompleto">
+            Idade: <input type="int" name="idade">
+            Sexo: <input type="char" name="sexo">
+</form>
+    </body>
+</html>
