a56342926ff00874c0e6dea71298c3ca138e6f7e
DessimA <60760405+DessimA@users.noreply.github.com>
2020-02-07 09:15:26 -0300

    Update ex02.php

--- lista_01_if/ex02.php
+++ lista_01_if/ex02.php
@@ -1,5 +1,24 @@
 <?php
 
-$nomeCompleto = $_GET["nomecompleto"];
+$N1 = $_POST["n1"];
+$N2 = $_POST["n2"];
 
-echo "<h1> Bem vindo, $nomeCompleto!</h1>";
+$soma = $N1+$N2;
+
+if($soma > 20){
+    
+        echo "$soma maior do que 20<br/>";
+        $soma+=8;
+        echo "Então recebe mais 8, agr igual a $soma";
+    }
+    else if($soma <= 20){
+        echo "$soma menor/igual a 20<br/>";
+        $soma-=5;
+        echo "Então decresce 5, agr igual a $soma";
+    }
+    else
+    {
+        echo "Valor invalido";
+    }
+
+
